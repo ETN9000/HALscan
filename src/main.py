cbdebf665afc635cde5dff136cba90420feaa327
ETN9000 <etn9000@protonmail.com>
2021-02-27 04:19:47 -0600

    Added interface checking

--- src/main.py
+++ src/main.py
@@ -3,7 +3,10 @@ from time import time, ctime
 import os
 from netifaces import AF_INET, AF_INET6, AF_LINK, AF_PACKET, AF_BRIDGE
 import netifaces as netif
+import logging
 
+# Global variables
+initTime = time()
 
 def startup():
     def checkroot():
@@ -13,16 +16,44 @@ def startup():
         else:
             print("[✓] User has root/sudo permissions")
     def interfacechk():
-        interfaces = netif.interfaces()
-        if not interfaces:
+        interfacelist = netif.interfaces()
+        if not interfacelist:
             print("[✗] No interfaces available")
+            exit()
         else:
-            print("[✓] Available interfaces:", *interfaces, sep=" ")
+            print("[✓] Available interfaces:", *interfacelist, sep=" ")
+
+    print("[+] HALscan initiated at: " + ctime(initTime))
+    print("[+] Performing startup checks")
 
     checkroot()
     interfacechk()
 
-initTime = time()
-print("[+] HALscan initiated at: " + ctime(initTime))
-print("[+] Performing startup checks")
+
+
+
+def netmapper():
+    print("[I] Beginning Network Mapping on")
+
+
+
+
+
+
+
+
+def main():
+    print("\n" + "/// HALscan Version 1.0 ///\n")
+
+    interface = input("Enter the interface you would like to use? %s: " % netif.interfaces() )
+    if any(word in interface for word in netif.interfaces()):
+        print("[✓] Valid choice")
+    else:
+        print("Invalid option")
+
+
+
+
+
 startup()
+main()
