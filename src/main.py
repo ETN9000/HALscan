ba14e02bd5b2177489d12607cede2142cc7dbaf0
ETN9000 <etn9000@protonmail.com>
2021-02-27 03:58:07 -0600

    Started adding startup checks

--- src/main.py
+++ src/main.py
@@ -1,10 +1,28 @@
 import socket
 from time import time, ctime
+import os
+from netifaces import AF_INET, AF_INET6, AF_LINK, AF_PACKET, AF_BRIDGE
+import netifaces as netif
 
 
 def startup():
-    initTime = time()
-    print(ctime(initTime))
+    def checkroot():
+        if os.geteuid() != 0:
+            print("[✗] User does not have root/sudo permissions")
+            exit()
+        else:
+            print("[✓] User has root/sudo permissions")
+    def interfacechk():
+        interfaces = netif.interfaces()
+        if not interfaces:
+            print("[✗] No interfaces available")
+        else:
+            print("[✓] Available interfaces:", *interfaces, sep=" ")
 
+    checkroot()
+    interfacechk()
 
+initTime = time()
+print("[+] HALscan initiated at: " + ctime(initTime))
+print("[+] Performing startup checks")
 startup()
